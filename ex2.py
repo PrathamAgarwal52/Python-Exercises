26623be3802eeedca964485e3fb95d8fc794fdbd
Pratham Agarwal <prathamagarwal523613@gmail.com>
2024-04-04 22:46:31 +0530

    exercise2 completed

---
+++ ex2.py
@@ -0,0 +1,5 @@
+# This is a comment, so you can read your program later.
+# anything after the # in that line is ignored by python.
+print("I could have code like this.") #and the comment after is ignored
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "this will not run."
