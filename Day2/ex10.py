bbf4b1eab1f24d665ae490dcdeb16772d738d083
Pratham Agarwal <prathamagarwal523613@gmail.com>
2024-04-05 18:31:21 +0530

    ex 10 completed escape sequence characters

---
+++ Day2/ex10.py
@@ -0,0 +1,20 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+#tab character is used to provide four spaces
+
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
+#fun code it prints all the values of these characters repeatedly and your system get crashed nice try sir.....but i killed that terminal
+#while True:
+    #for i in ["/","- ","|","\\","|"]:
+       # print ("%s\r" % i,)
