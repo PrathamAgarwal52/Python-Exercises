9b9e670e9c06a0e33a838c4390b1e0b5e33c70da
Pratham Agarwal <prathamagarwal523613@gmail.com>
2024-04-08 18:47:35 +0530

    ex34 completed ordinal and cardinal numbers

---
+++ Week2/Day5/ex34.py
@@ -0,0 +1,9 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+# first animal is at 0 and is a  bear
+# the third animal is at 2 and is a peacock
+# the animal at 1 is python
+#The animal at 3 is kangaroo
+#The fifth animal is at 4 and is whale
+#The animal at 2 is peacock
+# The sixth animal is at 5 and is platypus
+# The animal at 4 is whale
