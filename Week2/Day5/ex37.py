d59c1ed6b150f9741c4199e7f026b344adc86747
Pratham Agarwal <prathamagarwal523613@gmail.com>
2024-04-08 19:44:58 +0530

    ex37 completed

---
+++ Week2/Day5/ex37.py
@@ -0,0 +1,74 @@
+# Keywords and Statements:
+
+# - del: Deletes a reference to an object.
+# - from: Used for importing specific attributes or functions from a module.
+# - not: Negates a boolean value.
+# - while: Executes a block of code as long as a condition is true.
+# - as: Renames imported modules or bindings.
+# - elif: Short for "else if", used in conditional statements.
+# - global: Declares a variable as global within a function.
+# - or: Logical OR operator.
+# - with: Used for resource management (like opening files) with a guaranteed cleanup.
+# - assert: Used for debugging, raises an error if a condition is false.
+# - else:Used in conditional statements as a catch-all if previous conditions are not met.
+# - if: Conditional statement for branching program flow.
+# - pass:Placeholder indicating no action is taken.
+# - yield:Used in generator functions to produce a sequence of values.
+# - break: Exits from the innermost loop.
+# - except: Catches exceptions in try-except blocks.
+# - import: Used to import modules into a Python script.
+# - print: Outputs text to the console.
+# - class: Used to define a new class.
+# - exec: Executes dynamically created Python code.
+# - in: Checks if a value is present in a sequence.
+# - raise: Raises an exception manually.
+# - continue:Skips the rest of the current iteration of a loop.
+# - finally: Block of code executed regardless of whether an exception occurred or not.
+# - is: Checks if two variables refer to the same object.
+# - return:Exits a function and optionally returns a value.
+# - def: Defines a function.
+# - for: Iterates over elements of a sequence.
+# - lambda: Creates an anonymous function.
+# - try: Defines a block of code to be tested for errors.
+
+# Data Types:
+
+# - True: Boolean value representing true.
+# - False: Boolean value representing false.
+# - None: Represents the absence of a value.
+# - strings: Sequence of characters, created using quotes.
+# - numbers: Numeric data types, including integers and floating-point numbers.
+# - floats: Floating-point numbers.
+# - lists: Ordered collection of items.
+
+# String Escape Sequences: (Escape sequences are used to represent special characters in strings.)
+
+# - \\: Backslash.
+# - \': Single quote.
+# - \": Double quote.
+# - \a: Bell or alert.
+# - \b: Backspace.
+# - \f: Formfeed.
+# - \n: Newline.
+# - \r: Carriage return.
+# - \t: Tab.
+# - \v: Vertical tab.
+
+# String Formats: (Used to format strings with specific placeholders.)
+
+# - %d, %i, %o, %u, %x, %X: Integer placeholders.
+# - %e, %E, %f, %F, %g, %G: Floating-point placeholders.
+# - %c: Character placeholder.
+# - %r: Represents any Python object with its repr() method.
+# - %s: String placeholder.
+# - %%: Literal percent sign.
+
+# Operators: (Symbols used for operations.)
+
+# - Arithmetic Operators: +, -, *, **, /, //, %
+# - Comparison Operators: <, >, <=, >=, ==, !=
+# - Membership Operators: in
+# - Assignment Operators: =, +=, -=, *=, /=, //=, %=
+# - Logical Operators: and, or, not
+# - Identity Operators: is
+# - Bitwise Operators: &, |, ^, ~, <<, >>
