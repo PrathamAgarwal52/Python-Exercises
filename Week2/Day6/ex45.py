70c84b3159d7da417130abb32d5f50ceea98ae26
Pratham Agarwal <prathamagarwal523613@gmail.com>
2024-04-09 18:48:06 +0530

    ex45 completed game with different files

--- Week2/Day6/ex45.py
+++ Week2/Day6/ex45.py
@@ -2,11 +2,11 @@ from room import Room
 from game import GameRunner
 
 gold_room = Room("Gold Room", "You are in a room full of gold. There are exits to the north and east.")
-koi_pond_room = Room("Koi Pond Room", "You are in a room with a serene koi pond. The only exit is to the west.")
+danger_room = Room("Danger Room", "You are in a room with a ghost. The only exit is to the west.")
 
 
-gold_room.add_paths({'north': koi_pond_room})
-koi_pond_room.add_paths({'west': gold_room})
+gold_room.add_paths({'north': danger_room})
+danger_room.add_paths({'west': gold_room})
 
 
 game = GameRunner(start_room=gold_room)
